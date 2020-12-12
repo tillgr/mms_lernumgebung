3b165f6c0b0bfceb438d9a400a1ebe82afb4f931
zhannalytov <33961060+zhannalytov@users.noreply.github.com>
2020-12-12 19:29:29 +0100

    #33 ADD axes, FIX quantisation

--- Discrete-Cos-Transform/script.js
+++ Discrete-Cos-Transform/script.js
@@ -1,48 +1,51 @@
 var c = document.getElementById('myCanvas');
 
 function drawPlot() {
-  var ctx = c.getContext("2d");
-  ctx.clearRect(0, 0, c.width, c.height);
-  var signal = sfield[current_s].split(',').map(Number);
-  drawSpline(signal, '#0000ff', ctx);
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.width, c.height);
+    var signal = sfield[current_s].split(',').map(Number);
+    drawSpline(signal, '#0000ff', ctx);
 
-  var q_field = params['q_mode'].split(',').map(Number);
-  user_sfield = backtransformation(user_cfield, params['t_mode'], q_field);
-  drawSpline(user_sfield, '#ff0000', ctx);
+    var q_field = params['q_mode'].split(',').map(Number);
+    user_sfield = backtransformation(user_cfield, params['t_mode'], q_field);
+    drawSpline(user_sfield, '#ff0000', ctx);
 
-  renderBoard();
+    renderBoard();
 }
 
 function drawSpline(points, color, ctx) {
-  ctx.strokeStyle = color;
-  points = rescale(points)
-  ctx.curve(points);
-  ctx.stroke();
+    ctx.strokeStyle = color;
+    points = rescale(points)
+    ctx.curve(points);
+    ctx.stroke();
 
-  for (var i = 0; i <= points.length; i += 2){
-    ctx.beginPath();
-    ctx.arc(points[i], points[i+1], 3, 0, 2 * Math.PI);
-    ctx.fillStyle = color;
-    ctx.fill()
-  }
+    for (var i = 0; i <= points.length; i += 2){
+        ctx.beginPath();
+        ctx.arc(points[i], points[i+1], 3, 0, 2 * Math.PI);
+        ctx.fillStyle = color;
+        ctx.fill()
+    }
 }
 
 function rescale(points) {
-  var newPoints = new Array(points.length * 2);
+    // points contains y values of the coordinates
+    var newPoints = new Array(points.length * 2);
+    var height = c.height;
 
-  for (var i = 0; i < points.length; i++){
-    newPoints[i * 2] = 100 * i;
-    newPoints[i * 2 + 1] = (500 - points[i] * 2);
-  }
-  return newPoints;
+    for (var i = 0; i < points.length; i++){
+        newPoints[i * 2] = 100 * i;                         // Insert x value
+        var y_val = Math.min(Math.max(points[i], 0), 255);  // Limit to (0, 255)
+        newPoints[i * 2 + 1] = (height - y_val * 2);        // Scale to canvas size
+    }
+    return newPoints;
 }
 
 function magic() {
-  var signal = sfield[current_s].split(',').map(Number);
-  var q_field = params['q_mode'].split(',').map(Number);
-  var cfield = transformation(signal, params['t_mode'], q_field);
-  reset(cfield);
-  drawPlot();
+    var signal = sfield[current_s].split(',').map(Number);
+    var q_field = params['q_mode'].split(',').map(Number);
+    var cfield = transformation(signal, params['t_mode'], q_field);
+    reset(cfield);
+    drawPlot();
 }
 
 // ----------------- GUI -----------------
@@ -50,33 +53,34 @@ function magic() {
 //setup
 var t_mode = ['DCT', 'DFT'];
 var sfield = {1:'150, 170, 132, 185, 147, 190, 215, 220',
-		 2:'165, 185, 130, 190, 175, 196, 223, 199',
-		 3:' 55, 163, 180,  20, 202, 173,  97, 170',
-		 4:'143, 154, 160, 170, 211, 185, 190, 166',
-		 5:'130, 140, 172, 190, 193, 150, 180, 140',
-		 6:' 35,  64, 198, 180,  77, 141,  72, 135',
-		 7:'170, 190, 163, 140, 165, 132, 160, 140',
-		 8:'160,  40, 145,  35, 170, 199, 190,  29',
-		 9:'120,  80, 120,  80, 120,  80, 120,  80',
-		 10:'200, 100, 200, 100, 200, 100, 200, 100',
-		 11:'255,   0, 255,   0, 255,   0, 255,   0',
-		 12:'0,   0,   0,   0,   0,   0,   0,   0'};
+         2:'165, 185, 130, 190, 175, 196, 223, 199',
+         3:' 55, 163, 180,  20, 202, 173,  97, 170',
+         4:'143, 154, 160, 170, 211, 185, 190, 166',
+         5:'130, 140, 172, 190, 193, 150, 180, 140',
+         6:' 35,  64, 198, 180,  77, 141,  72, 135',
+         7:'170, 190, 163, 140, 165, 132, 160, 140',
+         8:'160,  40, 145,  35, 170, 199, 190,  29',
+         9:'120,  80, 120,  80, 120,  80, 120,  80',
+         10:'200, 100, 200, 100, 200, 100, 200, 100',
+         11:'255,   0, 255,   0, 255,   0, 255,   0',
+         12:'0,   0,   0,   0,   0,   0,   0,   0'};
 var current_s = 1;
 var q_mode = {'Do not quantise': '1, 1, 1, 1, 1, 1, 1, 1',
-    'Divide by 2': '2, 2, 2, 2, 2, 2, 2, 2',
-    'Divide by 5': '1, 1, 1, 1, 1, 1, 1, 1',
-    'Divide by 8': '1, 1, 1, 1, 1, 1, 1, 1',
-    'Divide by 16': '1, 1, 1, 1, 1, 1, 1, 1',
-    'JPEG Standard': '1, 1, 1, 1, 1, 1, 1, 1'};
+        'Divide u\'s by 2': '2, 2, 2, 2, 2, 2, 2, 2',
+        'Divide u\'s by 5': '5, 5, 5, 5, 5, 5, 5, 5',
+        'Divide u\'s by 10': '10, 10, 10, 10, 10, 10, 10, 10',
+        'Divide u\'s by 25': '25, 25, 25, 25, 25, 25, 25, 25',
+        'Divide u\'s by 50': '50, 50, 50, 50, 50, 50, 50, 50',
+        'JPEG Standard': '16, 11, 10, 16, 24, 40, 51, 61'};
 var user_sfield = [0, 0, 0, 0, 0, 0, 0, 0]
 var user_cfield = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var sliderControllers = new Array(user_cfield.length);
 
 var params = {
-  't_mode': t_mode[0],
-  'q_mode': q_mode['Do not quantise'],
-  'reset': function() { reset([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) },
-  'magic': function() { magic() }
+    't_mode': t_mode[0],
+    'q_mode': q_mode['Do not quantise'],
+    'reset': function() { reset([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) },
+    'magic': function() { magic() }
 }
 
 const gui = new dat.GUI({autoPlace: false});
@@ -95,11 +99,11 @@ gui.add(params, 'magic');
 //onChange
 
 for (let i = 0; i < 16; i++) {
-  params['user_cfield' + i] = user_cfield[i];
-  sliderControllers[i] = gui.add(params, 'user_cfield' + i, -400, 400, 1).onChange((x) => {
-    user_cfield[i] = x;
-    drawPlot();
-  });
+    params['user_cfield' + i] = user_cfield[i];
+    sliderControllers[i] = gui.add(params, 'user_cfield' + i, -400, 400, 1).onChange((x) => {
+        user_cfield[i] = x;
+        drawPlot();
+    });
 }
 
 function change_mode(t_mode) {
@@ -124,12 +128,66 @@ function change_mode(t_mode) {
 }
 
 function reset(user_cfield) {
-  for (var i = 0; i < user_cfield.length; i++) {
-    sliderControllers[i].setValue(user_cfield[i]);
-  }
+    for (var i = 0; i < user_cfield.length; i++) {
+        sliderControllers[i].setValue(user_cfield[i]);
+    }
 }
+
+// ----------------- AXES -----------------
+
+function drawAxes() {
+    canvas_position = c.getBoundingClientRect();
+    svgElement = document.getElementById('mySVG');
+    svgElement.style.position = 'absolute';
+    svgElement.style.top = canvas_position.top - 25;
+    svgElement.style.left = canvas_position.left - 25;
+    const margin = {top: 24, bottom: 25, left: 25, right: 25};
+    graph = document.getElementById('mySVG');
+	var width = graph.width.baseVal.value;
+	var height = graph.height.baseVal.value;
+    graph = d3.select("#mySVG").attr("viewBox", [0, 0, width, height]);
+
+    x = d3.scaleLinear()
+		.domain([0, 7])
+		.range([margin.left, width - margin.right]);
+	y = d3.scaleLinear()
+		.domain([0, 255])
+		.range([height - margin.bottom, margin.top]);
+
+    var xAxis = d3.axisBottom(x)
+		.ticks(8);
+	var yAxis = d3.axisLeft(y)
+		.ticks(5);
+
+	//draw x-axis
+	graph.append("g")
+		.attr("class", "xAxis")
+		.attr("transform", "translate(0," + y(0) + ")")
+		.call(xAxis);
+	graph.append("text")
+		.style("font", "16px times")
+		.style("fill", "green")
+		.attr("class", "xAxis")
+		.attr("transform", "translate(" + (width - margin.right + 10) + "," + y(0) + ")")
+		.text("X");
+
+	//draw y-axis
+	graph.append("g")
+		.attr("class", "yAxis")
+		.attr("transform", "translate(" + (margin.left) + ", 0)")
+		.call(yAxis);
+	graph.append("text")
+		.style("font", "16px times")
+		.style("fill", "green")
+		.attr("class", "yAxis")
+		.attr("transform", "translate(" + (margin.left / 2 - 5) + "," + (margin.top - 10) + ")")
+		.text("Grayscale");
+}
+
+// ----------------- INIT -----------------
 
 document.addEventListener("DOMContentLoaded", function(){
     change_mode(params['t_mode']);
     drawPlot();
+    drawAxes();
 });
--- Discrete-Cos-Transform/transform.js
+++ Discrete-Cos-Transform/transform.js
@@ -1,82 +1,82 @@
 function transformation(sfield, transformationMode, quantisefield) {
-  var cfield = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  var x, u;
-  var N = 8;
-  var Cu;
-  var sum, real, imagine;
-  var sqrt2 = Math.sqrt(2);
+    var cfield = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var x, u;
+    var N = 8;
+    var Cu;
+    var sum, real, imagine;
+    var sqrt2 = Math.sqrt(2);
 
-  if (transformationMode === 'DCT') {
-    for (u = 0; u < 8; u++) {
-      if (u == 0) {
-        Cu = 1 / sqrt2;
-      } else {
-        Cu = 1;
-      }
-      sum = 0;
-      for (x = 0; x < 8; x++) {
-        sum += (sfield[x] - 128) * Math.cos((2*x+1) * u * Math.PI/16);
-      }
-      cfield[u * 2] = Math.round(sum * 0.5 * Cu);
-    }
+    if (transformationMode === 'DCT') {
+        for (u = 0; u < 8; u++) {
+            if (u == 0) {
+                Cu = 1 / sqrt2;
+            } else {
+                Cu = 1;
+            }
+            sum = 0;
+            for (x = 0; x < 8; x++) {
+                sum += (sfield[x] - 128) * Math.cos((2*x+1) * u * Math.PI/16);
+            }
+            cfield[u * 2] = Math.round(sum * 0.5 * Cu);
+        }
 
-    for (u = 0; u < 8; u++) {
-      cfield[u * 2] = Math.round(cfield[u * 2] / quantisefield[u]);
+        for (u = 0; u < 8; u++) {
+            cfield[u * 2] = Math.round(cfield[u * 2] / quantisefield[u]);
+        }
+    } else if (transformationMode === 'DFT') {
+        for (u = 0; u < 8; u++) {
+            real = 0;
+            imagine = 0;
+
+            for (x = 0; x < 8; x++) {
+                real += sfield[x] * Math.cos(2 * Math.PI * x * u/N);
+                imagine -= sfield[x] * Math.sin(2 * Math.PI * x * u/N);
+            }
+
+            cfield[u * 2] = Math.round(real);
+            cfield[u*2 + 1] = Math.round(imagine);
+        }
     }
-  } else if (transformationMode === 'DFT') {
-    for (u = 0; u < 8; u++) {	
-				real = 0;
-				imagine = 0;
-								
-				for (x = 0; x < 8; x++) {											
-					real += sfield[x] * Math.cos(2 * Math.PI * x * u/N);
-					imagine -= sfield[x] * Math.sin(2 * Math.PI * x * u/N);							
-				}
-				
-				cfield[u * 2] = Math.round(real);
-				cfield[u*2 + 1] = Math.round(imagine);
-		}
-  } 
-  return cfield;
+    return cfield;
 }
 
-function backtransformation(usercfield, transformationMode, quantisefield) {	
-  var usersfield = [0, 0, 0, 0, 0, 0, 0, 0];
-  var x, u;
-  var N = 8;
-  var Cu = 0;
-  var sum, real, imagine;
-  var sqrt2 = Math.sqrt(2);
-          
-  if (transformationMode === 'DCT') {
-    for (u = 0; u <= 7; u++) {
-      usercfield[u * 2] *= quantisefield[u];	
-    }
-			
-    for (x = 0; x <= 7; x++) {		
-      sum = 0;
-      for (u = 0; u <= 7; u++) {
-        if (u == 0) {
-          Cu = 1 / sqrt2;
-        } else {
-          Cu = 1;
-        }
-        sum = sum + 0.5 * Cu * usercfield[u * 2] * Math.cos(Cu * (2*x + 1) * u * Math.PI/16);
-      }
-      usersfield[x] = Math.round(sum) + 128;				
-    }     
-  } else if (transformationMode === 'DFT') { 
-    for (x = 0; x <= 7; x++) {
-      real = 0;
-      imagine = 0;
-              
-      for (u = 0; u <= 7; u++) {
-        real += usercfield[u * 2] * Math.cos(2 * Math.PI * x * u/N);
-        imagine += usercfield[u*2 + 1] * Math.sin(2 * Math.PI * x * u/N);
-      }
-      usersfield[x] = Math.round((real - imagine) / N);
-    }
-  }
+function backtransformation(usercfield, transformationMode, quantisefield) {
+    var usersfield = [0, 0, 0, 0, 0, 0, 0, 0];
+    var x, u;
+    var N = 8;
+    var Cu = 0;
+    var sum, real, imagine;
+    var sqrt2 = Math.sqrt(2);
 
-  return usersfield;
+    if (transformationMode === 'DCT') {
+        for (u = 0; u <= 7; u++) {
+            usercfield[u * 2] *= quantisefield[u];
+        }
+
+        for (x = 0; x <= 7; x++) {
+            sum = 0;
+            for (u = 0; u <= 7; u++) {
+                if (u == 0) {
+                    Cu = 1 / sqrt2;
+                } else {
+                    Cu = 1;
+                }
+                sum = sum + 0.5 * Cu * usercfield[u * 2] * Math.cos(Cu * (2*x + 1) * u * Math.PI/16);
+            }
+            usersfield[x] = Math.round(sum) + 128;
+        }
+    } else if (transformationMode === 'DFT') {
+        for (x = 0; x <= 7; x++) {
+            real = 0;
+            imagine = 0;
+
+            for (u = 0; u <= 7; u++) {
+                real += usercfield[u * 2] * Math.cos(2 * Math.PI * x * u/N);
+                imagine += usercfield[u*2 + 1] * Math.sin(2 * Math.PI * x * u/N);
+            }
+            usersfield[x] = Math.round((real - imagine) / N);
+        }
+    }
+
+    return usersfield;
 }
