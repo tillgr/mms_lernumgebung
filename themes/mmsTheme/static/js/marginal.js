b171d06c5c7ff5e9bbfd45a280dfd274c06a8fea
MirkoJa <mirko.jantschke@gmx.de>
2020-12-17 10:16:48 +0100

    #41 ADD added image shortcodes to all sites and added some additional images

--- themes/mmsTheme/static/js/marginal.js
+++ themes/mmsTheme/static/js/marginal.js
@@ -1,41 +1,45 @@
-var headings = $('main').find('h2,h3,h4,h5,h6');
+$(document).ready(()=> {
+  var headings = $('main').find('h2,h3,h4,h5,h6');
 
-if (headings.length > 0) {
-  headings.addClass('h-marginal');
+  if (headings.length > 0) {
+    headings.addClass('h-marginal');
 
-  var $result = $('<div/>');
-  var curDepth = 0;
+    var $result = $('<div/>');
+    var curDepth = 1;
 
-  $('.h-marginal').each(function(index) {
-    let headingID = "marg-heading" + index;
+    $('.h-marginal').each(function(index) {
+      let headingID = "marg-heading" + index;
 
-    var $li = $('<li/>').html($('<a/>').text($(this).text()).attr("href", "#" + headingID));
-    $(this).attr("id", headingID);
+      var $li = $('<li/>').html($('<a/>').text($(this).text()).attr("href", "#" + headingID));
+      $(this).html("<span id='" + headingID + "'></span>"+ $(this).html());
+      // offset the span to display the heading below the sticky topnav
+      $(this).find('span').css({"position":"relative","top":"-4rem"});
 
-    var depth = parseInt(this.tagName.substring(1));
+      var depth = parseInt(this.tagName.substring(1));
 
-    if (depth > curDepth) { // going deeper
+      if (depth > curDepth) { // going deeper
 
-      $result.append($('<ul/>').append($li).addClass('list-unstyled'));
-      $result = $li;
+        $result.append($('<ul/>').append($li).addClass('list-unstyled'));
+        $result = $li;
 
-    } else if (depth < curDepth) { // going shallower
+      } else if (depth < curDepth) { // going shallower
 
-      $result.parents('ul:eq(' + (curDepth - depth - 1) + ')').append($li);
-      $result = $li;
+        $result.parents('ul:eq(' + (curDepth - depth) + ')').append($li);
+        $result = $li;
 
-    } else { // same level
+      } else { // same level
 
-      $result.parent().append($li);
-      $result = $li;
+        $result.parent().append($li);
+        $result = $li;
 
-    }
+      }
 
-    curDepth = depth;
-  });
+      curDepth = depth;
+    });
 
-  $result = $result.parents('ul:last');
-  $('.marginal .result').html($result);
-} else {
-  $('.marginal .result').html("<p>Keine Navigation vorhanden</p>");
-}
+    $result = $result.parents('ul:last');
+    $('.marginal .result').html($result);
+  } else {
+    $('.marginal .result').html("<p>Keine Navigation vorhanden</p>");
+  }
+});
